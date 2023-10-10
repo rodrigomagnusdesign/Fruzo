a43c3ecf69cea216c614c141227372685b65c42e
Pedro H Moreira <pedrohmoreira.office@gmail.com>
2023-10-10 20:35:14 -0300

    Adicionado API e Input no All

--- Código/All/all.js
+++ Código/All/all.js
@@ -1,8 +1,20 @@
 
-let botao = document.getElementById("botao")
+let botao = document.getElementsByClassName("botao")
 botao.addEventListener("click", acaoClique)
 
 function acaoClique() {
     console.log("Resposta ao evento BOTÃO");
+    alert("Entre em contato pelo nosso WhatsApp, temos desconto de 5%!");
+
 }
-alert("Voce clicou no botão")
+
+let fruits;
+
+fetch('https://www.fruityvice.com/')
+    .then(response => response.json())
+    .then(data => exibir(data))
+
+function exibir(dados) {
+    fruits = dados
+    console.log(fruits)
+}
